220da41e0d84980c6d2eecbe4ceaaf5bf0c6ed9b
Harshvardhan295 <harshvardhankhajuria14@gmail.com>
2024-09-27 21:14:32 +0530

    Updated code
    Displays absent roll numbers. by reading total number of students by the user.

--- index.js
+++ index.js
@@ -1,15 +1,50 @@
-let saveEl = document.getElementById("save-el")
-let countEl = document.getElementById("count-el")
-let count = 0
+let displayEl = document.getElementById("display-el");
+let countEl = document.getElementById("count-el");
+let totalElInput = document.getElementById("total-el");
+let totalEl = 0;
+
+let count = 1; 
+let presentStudents = []; 
+
+function storeTotal() {
+    totalEl = parseInt(totalElInput.value); 
+    count = 1; 
+    countEl.textContent = "Roll Number: " + count; 
+    displayEl.textContent = "Absent Roll Numbers: "; 
+    presentStudents = []; 
+}
 
 function increment() {
-    count += 1
-    countEl.textContent = count //textContent is replacement for innertext
+    if (count <= totalEl) {
+        presentStudents.push(count); 
+        count += 1; 
+        countEl.textContent = "Roll Number: " + count; 
+    } else {
+        alert("Roll Number " + count + " does not exist. You have taken the attendance of all students.");
+    }
 }
 
 function save() {
-    let countStr = count + " - "
-    saveEl.textContent += countStr
-    countEl.textContent = 0
-    count = 0
+    if (count > totalEl) {
+        highlightAbsentNumbers(); 
+    } else if (count >= 1) { // Exclude the initial roll number
+        let countStr = (count) + " , "; // Store the last valid count
+        displayEl.textContent += countStr; 
+        count += 1; 
+        countEl.textContent = "Roll Number: " + count; 
+    } 
+}
+
+function highlightAbsentNumbers() {
+    let absentNumbers = [];
+    for (let i = 1; i <= totalEl; i++) {
+        if (!presentStudents.includes(i)) { 
+            absentNumbers.push(i); 
+        }
+    }
+    if (absentNumbers.length ===0) {
+    displayEl.textContent = "All students are present.";
+    }else {
+        displayEl.textContent = "Absent Roll Numbers: " + absentNumbers.join(", ");
+    } 
 }
